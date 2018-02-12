986ee8a6eb5f3e8cfc8ba04508aca9f31459aba3
Jonas <jonas.mueller.contact@gmail.com>
2018-02-12 21:11:02 +0100

    Added a bunch of failsafes to adapt to different terrain. Tweaked some numbers

--- robot/universal_farm.lua
+++ robot/universal_farm.lua
@@ -34,7 +34,7 @@ local m = component.modem
 
 
 -- How long the robot will wait between farming operations. Can be changed as needed.
-local sleepInterval = 60
+local sleepInterval = 120
 
 -- Main farm loop
 function farmLoop()
@@ -46,24 +46,43 @@ function farmLoop()
     -- Move into position
     robot.up()
     robot.forward()
-    -- Farm first strip
-    farmStrip()
     -- Keep farming strips until hitting the container
     isNotPassable, state = robot.detect()
-    while not checkForContainer() do
+    while true do
         -- Turn & move the right way at the end of a strip
-        if math.fmod(rows, 2) == 0 then
-            robot.turnRight()
-            robot.forward()
-            robot.turnRight()
-            robot.forward()
-        else
-            robot.turnLeft()
-            robot.forward()
-            robot.turnLeft()
-            robot.forward()
+        if not farmStrip(rows) then
+            if checkForContainer() then
+                print("Found chest, breaking...")
+                break
+            end
+            print("Turning")
+            if math.fmod(rows, 2) == 0 then
+                robot.turnRight()
+                -- Failsafe if something blocks path
+                if not robot.forward() then
+                    robot.turnLeft()
+                    robot.back()
+                    robot.turnRight()
+                    robot.forward()
+                    robot.turnRight()
+                else
+                    robot.turnRight()
+                    robot.forward()
+                end
+            else
+                robot.turnLeft()
+                if not robot.forward() then
+                    robot.turnRight()
+                    robot.back()
+                    robot.turnLeft()
+                    robot.forward()
+                    robot.turnLeft()
+                else
+                    robot.turnLeft()
+                    robot.forward()
+                end
+            end
         end
-        farmStrip()
         isNotPassable, state = robot.detect()
         rows = rows + 1
     end
@@ -71,23 +90,38 @@ function farmLoop()
 end
 
 -- While above crops (aka "passable"), harvest, replant and move forward
-function farmStrip()
+function farmStrip(row)
+    print("Farming row: "..row)
     isNotPassable, state = robot.detectDown()
+    isAligned = false
     while state == "passable" do
         robot.swingDown()
         robot.suckDown()
         replantCrop()
         if not robot.forward() then
+            print("Making sharp turn")
+            if math.fmod(rows, 2) == 0 then
+                robot.turnRight()
+                robot.forward()
+                robot.turnRight()
+            else
+                robot.turnLeft()
+                robot.forward()
+                robot.turnLeft()
+            end
+            isAligned = true
             break
         end
         isNotPassable, state = robot.detectDown()
     end
+    print("Finished row")
+    return isAligned
 end
 
 -- Drop all crops into container below and return to starting position
 function dropAndReturn(distance)
     itemcount = 0
-    for i = 1, 4 do
+    for i = 1, 8 do
         robot.select(i)
         slotcount = robot.count(i)
         itemcount = itemcount + slotcount
