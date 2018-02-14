7b26f48ecf2f6442eb0ad848ae827608d45395a4
Jonas <jonas.mueller.contact@gmail.com>
2018-02-15 00:50:19 +0100

    Added functions for adding & registering buttons + handling of their trigger-functions

--- lib/glasses_helper.lua
+++ lib/glasses_helper.lua
@@ -3,6 +3,7 @@ local comp = require "component"
 local g = comp.glasses
 
 local ghelper = {}
+local buttons = {}
 
 function ghelper.infoText(text, x, y , scale, color)
     local label = g.addTextLabel()
@@ -69,6 +70,23 @@ function ghelper.bgBox(x, y, w, h, primary_color, secondary_color)
         return label
     end
 
+    function bg_group.addButton(text, w, h, x, y, scale, color)
+        local button = g.addRect()
+        button.setSize(w, h)
+        button.setPosition((bg_group["root"][1] + x), (bg_group["root"][2] + y))
+        button.setColor(color[1], color[2] , color[3])
+
+        local label = g.addTextLabel()
+        local xspot = math.floor((x + x+w + bg_group["root"][1] - string.len(text)) /2) + 1
+        local yspot = math.floor((bg_group["root"][2] + y + (h/2))) + (2/scale)
+        label.setPosition(xspot, yspot)
+        label.setColor(0.1, 0.1, 0.1)
+        label.setScale(scale)
+        label.setText(text)
+
+        return button
+    end
+
     return bg_group
 end
 
@@ -83,6 +101,35 @@ function ghelper.cube(x, y, z, scale, color, alpha, xray, distance)
     cube.setViewDistance(distance)
 
     return cube
+end
+
+function ghelper.registerButton(name, object, func, func_args)
+    buttons[name] = {}
+    buttons[name]["func"] = func
+    buttons[name]["func_args"] = func_args
+    x, y = object.getPosition()
+    buttons[name]["xmin"], buttons[name]["ymin"] = x, y
+    w, h = object.getSize()
+    buttons[name]["xmax"], buttons[name]["ymax"] = x + w, y + h
+    print("Registered "..name.." at X: "..buttons[name]["xmin"].."-"..buttons[name]["xmax"].." and Y: "..buttons[name]["ymin"].."-"..buttons[name]["ymax"])
+end
+
+function ghelper.handleClick(_, _, _, x, y, _, _)
+    print("Got click: "..x..":"..y)
+    for _, data in pairs(buttons) do
+        print("Checking")
+        if y>=data["ymin"] and  y <= data["ymax"] then
+            if x>=data["xmin"] and x<= data["xmax"] then
+                print("Got one!")
+                if data["func_args"] ~= nil then
+                    data["func"](data["func_args"])
+                else
+                    data["func"]()
+                end
+                return true
+            end
+        end
+    end
 end
 
 return ghelper
