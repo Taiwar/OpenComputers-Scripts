bd42fe4f87e7837d617ad33935f3a4c0d9630f2b
Jonas <jonas.mueller.contact@gmail.com>
2018-02-15 16:19:00 +0100

    Rewrote OCPointer handling

--- holo_hud/hud_manager.lua
+++ holo_hud/hud_manager.lua
@@ -1,13 +1,12 @@
-local comp = require "component"
-local event = require "event"
-local serialization = require "serialization"
-local computer = require "computer"
-local term = require "term"
+local comp = require("component")
+local event = require("event")
+local serialization = require("serialization")
+local computer = require("computer")
+local term = require("term")
 
 local g = comp.glasses
 local m = comp.modem
 
-g.removeAll()
 local last_cmd = ""
 local last_msg = ""
 
@@ -22,12 +21,14 @@ local tab_functions = {
 }
 
 function switchTo(script_name)
-    computer.pushSignal("closeWidget")
+    print("Switching to "..script_name)
+    event.push("closeWidget")
     os.execute(script_name)
 end
 
 function cleanExit()
-    computer.pushSignal("closeWidget")
+    print("Exiting")
+    event.push("closeWidget")
     g.removeAll()
     event.ignore("modem_message")
     m.close(80)
@@ -50,9 +51,16 @@ function executeFunction(_, _, _, port, _, packet)
     print(last_cmd)
 end
 
+function executeSwitch(_, target)
+    tab_functions[target]()
+end
+
 print("startup")
+g.removeAll()
 m.open(80)
 m.open(8000)
 m.open(8001)
+event.listen("requestSwitch", executeSwitch)
 event.listen("modem_message", executeFunction)
 event.listen("interrupted", cleanExit)
+switchTo("home_hud.lua")
